a25322ef10f7a707bf2b3a9990dc05c146e122be
Mohit_jha <mohitjha1511@gmail.com>
2020-09-08 02:37:43 +0530

    Updates

---
+++ precache-manifest.be3ea8d9a2125afa753010c91b6879c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d860309627c0b1aaaf6abe60748552f",
+    "url": "/Keep-Notes-App-in-React/index.html"
+  },
+  {
+    "revision": "2d744185e182e00e8918",
+    "url": "/Keep-Notes-App-in-React/static/js/2.1799f166.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Keep-Notes-App-in-React/static/js/2.1799f166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c1fb5e5f26b8175a13",
+    "url": "/Keep-Notes-App-in-React/static/js/main.f442381f.chunk.js"
+  },
+  {
+    "revision": "f21f1f26acfd9ad06100",
+    "url": "/Keep-Notes-App-in-React/static/js/runtime-main.472f4bc8.js"
+  }
+]);
